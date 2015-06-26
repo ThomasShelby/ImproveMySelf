d6d8466eeddde2e8de0776a39af13cbc408b3f67
Thomas Shelby <mikola1693@gmail.com>
2015-06-27 00:52:10 +0300

    VERSION_2(Input_method+toString)

--- src/TestToString.java
+++ src/TestToString.java
@@ -1,6 +1,6 @@
 //====================================================================================================
 //====================================VERSION_1(with"toString")=======================================
-public class TestToString {
+/*public class TestToString {
 
 	private String name;
 	private String address;
@@ -19,5 +19,32 @@ public class TestToString {
 	public static void main(String[] args) {
 		TestToString ob=new TestToString("Mykola","Lviv", 21);
 		System.out.println(ob);
+	}
+}*/
+
+//====================================================================================================
+//====================================VERSION_2(Input method+ "toString")=============================
+import java.util.Scanner;
+public class TestToString{
+	private String name;
+	private String address;
+	private int age;
+	
+	public void Input(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Your name: ");
+		name=sc.nextLine();
+		System.out.println("Enter Your address: ");
+		address=sc.nextLine();
+		System.out.println("Enter Your age: ");
+		age=sc.nextInt();
+	}
+	public String toString(){
+		return "Name: "+name+"\nAddress: "+address+"\nAge: "+age; 
+	}
+	public static void main(String[] args) {
+		TestToString dt=new TestToString();
+		dt.Input();
+		System.out.println(dt);
 	}
 }
